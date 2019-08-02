607011646d616a3c416a4aca1d5f7da9c93d5f1e
David Anticona <danticona@wollcorp.com>
2019-08-02 18:33:17 -0500

    agregando servicio forecast

--- src/com/wollcorp/restServices/ForecastService.java
+++ src/com/wollcorp/restServices/ForecastService.java
@@ -1,13 +1,23 @@
 package com.wollcorp.restServices;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.wollcorp.controladores.ForecastControlador;
 import com.wollcorp.dto.DataForecastDTO;
 
@@ -17,7 +27,6 @@ import com.wollcorp.dto.DataForecastDTO;
  * @version 1.0
  */
 @Path("/forecast")
-@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ForecastService {
 	
@@ -29,6 +38,7 @@ public class ForecastService {
 	 * @return Naves y Servicios
 	 */
 	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response getDataForecast(@HeaderParam("token") String token) {
 		
 		DataForecastDTO dataForecastDTO = forecastControlador.getDatosForecast(token);
@@ -44,6 +54,17 @@ public class ForecastService {
 		}
 		
 		
+		
+	}
+	
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response postFileForecast(@HeaderParam("token") String token,
+			File file) {
+		
+		System.out.println(file.toString());
+
+	    return Response.status(200).entity("Recibido").build();
 		
 	}
 	
